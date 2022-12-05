e971fd0613ee5c7e0ce69da26e388ad49cd57891
AnamarijaD <anamarijadolovac@gmail.com>
2022-12-05 23:39:51 +0100

    added user change pass

--- Client/src/app/user-personal-profile/user-personal-profile.component.ts
+++ Client/src/app/user-personal-profile/user-personal-profile.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-user-personal-profile',
   templateUrl: './user-personal-profile.component.html',
   styleUrls: ['./user-personal-profile.component.css']
 })
-export class UserPersonalProfileComponent {
+export class UserPersonalProfileComponent implements OnInit, AfterViewInit{
+  title = 'appBootstrap';
+  @ViewChild('mymodal') mymodal: ElementRef | undefined;
+  closeResult = '';
+
+constructor(private modalService: NgbModal) {}
+  ngAfterViewInit(): void {
+    this.open(this.mymodal);
+  }
+  ngOnInit(): void {}
+
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
 }
