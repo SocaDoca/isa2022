26e0759151f6f428a79b89c6832823896c8f8386
Milos Rankovic <rankovic.losmi11@gmail.com>
2022-12-08 20:04:38 +0100

    fix .sou

---
+++ MedicApp/Migrations/20221206224135_AddressRemove.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicApp.Migrations
+{
+    public partial class AddressRemove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Clinics");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Clinics",
+                type: "longtext",
+                nullable: true);
+        }
+    }
+}
