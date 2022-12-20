b94706a8f7054cf364a412f0dee82d2ef26f50ee
milos.rankovic <milos.rankovic@b-op.com>
2022-12-20 16:36:18 +0100

    ErrorHandler Middleware

---
+++ MedicApp/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Options;
+using System.Globalization;
+using System.Net;
+using System.Text;
+
+namespace MedicApp.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (error)
+                {
+                    case UnauthorizedException e:
+                        // custom application error
+                        response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        break;
+                    case KeyNotFoundException e:
+                        // not found error
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        // unhandled error
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = System.Text.Json.JsonSerializer.Serialize(error?.Message);
+                await response.WriteAsync(result);
+            }
+        }
+    }
+
+
+    public class UnauthorizedException : Exception
+    {
+        public UnauthorizedException() : base() { }
+
+        public UnauthorizedException(string message) : base(message) { }
+
+        public UnauthorizedException(string message, params object[] args)
+            : base(string.Format(CultureInfo.CurrentCulture, message, args))
+        {
+
+        }
+    }
+}
