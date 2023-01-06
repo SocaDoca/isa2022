03c8e296a120a836b28476e1121b18400918a632
AnamarijaD <anamarijadolovac@gmail.com>
2023-01-06 16:10:18 +0100

    added navbar-profile component

--- MedicApp/Integrations/UserIntegration.cs
+++ MedicApp/Integrations/UserIntegration.cs
@@ -69,6 +69,7 @@ namespace MedicApp.Integrations
                 FirstName = findUser.FirstName,
                 LastName = findUser.LastName,
                 Token = encrypterToken,
+                Role = findUser.Role,
             };
         }
 
