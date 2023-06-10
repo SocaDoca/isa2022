3ad9a44995e677c13fa8c23d3af519410891d6d4
DESKTOP-G2354BM\PC <rankovic.losmi11@gmail.com>
2023-06-10 14:28:39 +0200

    Add migrations

---
+++ MedicApp/Migrations/20230606180248_test1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicApp.Migrations
+{
+    public partial class test1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "WorkItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "WorkItemType",
+                table: "WorkItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkItemType",
+                table: "WorkItems");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "WorkItems",
+                type: "longtext",
+                nullable: false);
+        }
+    }
+}
