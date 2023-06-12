14864d1f14d9c2088fbde609651cf2d873f9eddb
AnamarijaD <anamarijadolovac@gmail.com>
2023-06-13 01:56:25 +0200

    fixes

--- Client/src/app/visitation-history-user/visitation-history-user.component.ts
+++ Client/src/app/visitation-history-user/visitation-history-user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ClinicService } from '../../service/clinic.service';
+import { UserService } from '../../service/user.service';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-visitation-history-user',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./visitation-history-user.component.css']
 })
 export class VisitationHistoryUserComponent {
+  id: any;
+  user!: User;
+  constructor(private userService: UserService, private route: ActivatedRoute) {
 
+  }
+
+  ngOnInit() {
+    this.loadHistory();
+  }
+
+  loadHistory() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.userService.getUser(this.id).subscribe(res => {
+      this.user = res;
+      console.log(this.user);
+    });
+  }
 }
