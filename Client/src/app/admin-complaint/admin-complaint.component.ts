4e1acd03445322eb0c771eb1d9d6d930368731f5
AnamarijaD <anamarijadolovac@gmail.com>
2023-06-12 02:12:32 +0200

    finished user complaints

--- Client/src/app/admin-complaint/admin-complaint.component.ts
+++ Client/src/app/admin-complaint/admin-complaint.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 import { ClinicService } from '../../service/clinic.service';
 import { Complaint } from '../model/Complaint';
 
@@ -9,13 +12,36 @@ import { Complaint } from '../model/Complaint';
 })
 export class AdminComplaintComponent {
   complaint: any;
+  @ViewChild('mymodal')
+  public mymodal: ModalDirective | undefined;
+  isModalOpen = false;
+  id: any;
+  selectedComplaintId: any;
 
-  constructor(private clinicService: ClinicService) {
+  openModal(complaintId: any) {
+    console.log(complaintId);
+    this.id = complaintId;
+    this.modalService.open(this.mymodal).result.then((result) => {
+
+    });
+  }
+
+  constructor(private clinicService: ClinicService, private route: ActivatedRoute, private modalService: NgbModal) {
     this.complaint = new Complaint({
       description: '',
       status: true,
       type: ''
     })
+  }
+
+  respond() {
+    this.clinicService.answerComplaint(this.id, this.complaint.answer).subscribe(res => {
+      if (res === true) {
+        this.modalService.dismissAll();
+        window.location.reload();
+      }
+     
+    });
   }
 
 
